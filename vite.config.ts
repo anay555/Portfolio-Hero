eca001a19c70900cb4bb2cd5531b972804ac0d2d
anay555 <ananysharmaofficial5@gmail.com>
2026-05-09 22:10:07 +0530

    Updated app with new features and bug fixes

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,16 @@ export default defineConfig({
       '@': path.resolve(__dirname, '.'),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
